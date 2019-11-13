f76a9297a5cc00df5a1095f0f9df7abba788ed0d
Stephen <stephen.kerr@docplanner.com>
2019-11-13 09:19:53 +0100

    feat: init

---
+++ src/lib/firestoreHook.js
@@ -0,0 +1,66 @@
+import { useEffect, useState } from "react";
+/*
+  eslint-disable react-hooks/rules-of-hooks
+*/
+export const firestoreQueryHook = collection => {
+  const [state, setState] = useState({
+    error: null,
+    loading: false,
+    data: null
+  });
+
+  useEffect(() => {
+    setState({
+      ...state,
+      loading: true
+    });
+    window.firebase
+      .firestore()
+      .collection(collection)
+      .orderBy("timestamp", "desc")
+      .onSnapshot(querySnapshot => {
+        let documents = [];
+        querySnapshot.forEach(function(doc) {
+          documents.push({
+            id: doc.id,
+            ...doc.data()
+          });
+
+          setState({
+            ...state,
+            loading: false,
+            data: documents
+          });
+        });
+      });
+  }, []);
+
+  return state;
+};
+
+export const firestoreMutationHook = (collection) => {
+  const [state, setState] = useState({
+    error: null,
+    loading: false
+  });
+
+  const postDocument = (data) => {
+    setState({
+      loading: true
+    })
+
+    window.firebase.firestore().collection(collection).add({
+      ...data,
+      timestamp: new Date().getTime()
+    }).then(() => {
+      setState({
+        loading: false
+      })
+    })
+  }
+
+  return {
+    ...state,
+    postDocument
+  }
+}
